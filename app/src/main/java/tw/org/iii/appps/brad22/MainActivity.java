f4fed40a4dd861f24838884a554ce69f77a56cf6
Brad <brad@brad.tw>
2019-08-11 15:24:51 +0800

    4

--- app/src/main/java/tw/org/iii/appps/brad22/MainActivity.java
+++ app/src/main/java/tw/org/iii/appps/brad22/MainActivity.java
@@ -1,7 +1,16 @@
 package tw.org.iii.appps.brad22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -14,16 +23,75 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private EditText num;
+    private LocationManager lmgr;
+    private MyListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    123);
+        }
+
+        lmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        listener = new MyListener();
+
         num = findViewById(R.id.num);
         webView = findViewById(R.id.webview);
         initWebView();
     }
+
+    //    @Override
+    //@SuppressLint("MissingPermission")
+    protected void onStart() {
+        super.onStart();
+        lmgr.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 1*1000,10, listener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        lmgr.removeUpdates(listener);
+    }
+
+    private class MyListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            Log.v("brad",lat + ", " + lng );
+            webView.loadUrl("javascript:moveTo(" + lat + ", " + lng + ")");
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
+
+
+
+
+
 
     private void initWebView(){
         WebViewClient webViewClient = new WebViewClient();
